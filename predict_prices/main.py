6279478970fe3760de422d9db03bc25bce810ac8
TonyLouwagie <tony.louwagie@gmail.com>
2024-08-04 14:47:55 -0500

    clean up

--- predict_prices/main.py
+++ predict_prices/main.py
@@ -21,6 +21,7 @@ lr = LinearRegression()
 ohe = False
 n_iter = 2
 
+# Initialize all model types
 rf = RandomForestRegressor()
 rf_param_grid = {
     "n_estimators": tuple(range(10, 1000)),
@@ -44,6 +45,7 @@ lasso = Lasso()
 ridge = Ridge()
 br = BayesianRidge()
 
+# 1. read in training data, perform data prep
 train_df = data_prep.load_data(train_fp)
 train_df = data_prep.eda_clean(train_df)
 train_df, _ = data_prep.clean_after_eda(train_df)
@@ -54,9 +56,7 @@ train_df = data_prep.categorical_transform(train_df, cat_enc)
 train_X, train_y = data_prep.split_x_y(train_df, tgt)
 train_X = train_X if include_categoricals else data_prep.drop_categoricals(train_X)
 
-
-
-
+# 2. Measure performance of all of our different models
 lr.fit(train_X, train_y)
 lr_param_scores = cross_validation.save_model_performance_parameters(lr, train_X, train_y, folds, ord_enc, cat_enc)
 
@@ -64,11 +64,11 @@ en.fit(train_X, train_y)
 en_param_scores = cross_validation.save_model_performance_parameters(en, train_X, train_y, folds, ord_enc, cat_enc)
 
 lasso.fit(train_X, train_y)
-lasso_param_scores = cross_validation.save_model_performance_parameters(lasso, train_X, train_y, folds, ord_enc, cat_enc)
-
+lasso_param_scores = cross_validation.save_model_performance_parameters(lasso, train_X, train_y, folds, ord_enc,
+                                                                        cat_enc)
 ridge.fit(train_X, train_y)
-ridge_param_scores = cross_validation.save_model_performance_parameters(ridge, train_X, train_y, folds, ord_enc, cat_enc)
-
+ridge_param_scores = cross_validation.save_model_performance_parameters(ridge, train_X, train_y, folds, ord_enc,
+                                                                        cat_enc)
 br.fit(train_X, train_y)
 br_param_scores = cross_validation.save_model_performance_parameters(br, train_X, train_y, folds, ord_enc, cat_enc)
 
@@ -81,11 +81,11 @@ xgb_param_scores = cross_validation.save_model_performance_parameters(xgb, train
 lgbm = cross_validation.bayes_cross_validation(lgbm, train_X, train_y, lgbm_param_grid, n_iter)
 lgbm_param_scores = cross_validation.save_model_performance_parameters(lgbm, train_X, train_y, folds, ord_enc, cat_enc)
 
+# 3. Compare performance and select best model
 models = [lr_param_scores, rf_param_scores, xgb_param_scores, lgbm_param_scores, en_param_scores, lasso_param_scores]
+param_scores = pd.DataFrame(models).sort_values('score', ascending=False)
 
-param_scores = pd.DataFrame(models).sort_values('score',ascending=False)
-
-print(param_scores[['model','score','standard_dev']])
+print(param_scores[['model', 'score', 'standard_dev']])
 
 # take only first row in case of ties (I don't care which model if they're tied)
 champ = param_scores[param_scores.score == param_scores.score.max()].reset_index()
@@ -93,6 +93,7 @@ champ_model = champ.model[0]
 champ_ord_enc = champ.ordinal_encoder[0]
 champ_cat_enc = champ.categorical_encoder[0]
 
+# 4. Read in test data and apply data prep from step 1
 test_df = data_prep.load_data(test_fp)
 test_df = data_prep.eda_clean(test_df)
 test_df, _ = data_prep.clean_after_eda(test_df)
@@ -100,6 +101,7 @@ test_df = data_prep.ordinal_transform(test_df, champ_ord_enc)
 test_df = data_prep.categorical_transform(test_df, champ_cat_enc)
 test_X = test_df if include_categoricals else data_prep.drop_categoricals(test_df)
 
+# 5. predict test data and output to csv
 # the model was trained against log transformed target. Invert log for predictions
 test_y = np.exp(champ_model.predict(test_X))
 print(test_X.head())
@@ -107,5 +109,3 @@ print(test_X.head())
 test_y = pd.DataFrame(test_y, columns=['SalePrice'], index=test_X.index)
 
 test_y.to_csv('prediction.csv')
-
-#print(test_y.head())
