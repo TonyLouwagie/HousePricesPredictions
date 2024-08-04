939174ad55a0e622618acb03514b8b15155a70f3
Tony Louwagie <tony.louwagie@gmail.com>
2024-08-04 14:49:12 -0500

    Factor out repetitive code (#51)

--- predict_prices/main.py
+++ predict_prices/main.py
@@ -21,6 +21,7 @@ lr = LinearRegression()
 ohe = False
 n_iter = 2
 
+# Initialize all model types
 rf = RandomForestRegressor()
 rf_param_grid = {
     "n_estimators": tuple(range(10, 1000)),
@@ -44,6 +45,7 @@ lasso = Lasso()
 ridge = Ridge()
 br = BayesianRidge()
 
+# 1. read in training data, perform data prep
 train_df = data_prep.load_data(train_fp)
 train_df = data_prep.eda_clean(train_df)
 train_df, _ = data_prep.clean_after_eda(train_df)
@@ -54,91 +56,36 @@ train_df = data_prep.categorical_transform(train_df, cat_enc)
 train_X, train_y = data_prep.split_x_y(train_df, tgt)
 train_X = train_X if include_categoricals else data_prep.drop_categoricals(train_X)
 
+# 2. Measure performance of all of our different models
 lr.fit(train_X, train_y)
-lr_aggregate_scores = cross_validation.cross_val_aggregate(lr, train_X, train_y, folds)
-lr_param_scores = {
-    "model": lr,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": lr_aggregate_scores["Mean"],
-    "standard_dev": lr_aggregate_scores["Standard Deviation"]
-}
+lr_param_scores = cross_validation.save_model_performance_parameters(lr, train_X, train_y, folds, ord_enc, cat_enc)
 
 en.fit(train_X, train_y)
-en_aggregate_scores = cross_validation.cross_val_aggregate(en, train_X, train_y, folds)
-en_param_scores = {
-    "model": en,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": en_aggregate_scores["Mean"],
-    "standard_dev": en_aggregate_scores["Standard Deviation"]
-}
+en_param_scores = cross_validation.save_model_performance_parameters(en, train_X, train_y, folds, ord_enc, cat_enc)
 
 lasso.fit(train_X, train_y)
-lasso_aggregate_scores = cross_validation.cross_val_aggregate(lasso, train_X, train_y, folds)
-lasso_param_scores = {
-    "model": lasso,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": lasso_aggregate_scores["Mean"],
-    "standard_dev": lasso_aggregate_scores["Standard Deviation"]
-}
-
+lasso_param_scores = cross_validation.save_model_performance_parameters(lasso, train_X, train_y, folds, ord_enc,
+                                                                        cat_enc)
 ridge.fit(train_X, train_y)
-ridge_aggregate_scores = cross_validation.cross_val_aggregate(ridge, train_X, train_y, folds)
-ridge_param_scores = {
-    "model": ridge,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": ridge_aggregate_scores["Mean"],
-    "standard_dev": ridge_aggregate_scores["Standard Deviation"]
-}
-
+ridge_param_scores = cross_validation.save_model_performance_parameters(ridge, train_X, train_y, folds, ord_enc,
+                                                                        cat_enc)
 br.fit(train_X, train_y)
-br_aggregate_scores = cross_validation.cross_val_aggregate(br, train_X, train_y, folds)
-br_param_scores = {
-    "model": br,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": br_aggregate_scores["Mean"],
-    "standard_dev": br_aggregate_scores["Standard Deviation"]
-}
+br_param_scores = cross_validation.save_model_performance_parameters(br, train_X, train_y, folds, ord_enc, cat_enc)
 
 rf = cross_validation.bayes_cross_validation(rf, train_X, train_y, rf_param_grid, n_iter)
-rf_aggregate_scores = cross_validation.cross_val_aggregate(rf, train_X, train_y, folds)
-rf_param_scores = {
-    "model": rf,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": rf_aggregate_scores["Mean"],
-    "standard_dev": rf_aggregate_scores["Standard Deviation"]
-}
+rf_param_scores = cross_validation.save_model_performance_parameters(rf, train_X, train_y, folds, ord_enc, cat_enc)
 
 xgb = cross_validation.bayes_cross_validation(xgb, train_X, train_y, xgb_param_grid, n_iter)
-xgb_aggregate_scores = cross_validation.cross_val_aggregate(xgb, train_X, train_y, folds)
-xgb_param_scores = {
-    "model": xgb,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": xgb_aggregate_scores["Mean"],
-    "standard_dev": xgb_aggregate_scores["Standard Deviation"]
-}
+xgb_param_scores = cross_validation.save_model_performance_parameters(xgb, train_X, train_y, folds, ord_enc, cat_enc)
 
 lgbm = cross_validation.bayes_cross_validation(lgbm, train_X, train_y, lgbm_param_grid, n_iter)
-lgbm_aggregate_scores = cross_validation.cross_val_aggregate(lgbm, train_X, train_y, folds)
-lgbm_param_scores = {
-    "model": lgbm,
-    "ordinal_encoder": ord_enc,
-    "categorical_encoder": cat_enc,
-    "score": lgbm_aggregate_scores["Mean"],
-    "standard_dev": lgbm_aggregate_scores["Standard Deviation"]
-}
+lgbm_param_scores = cross_validation.save_model_performance_parameters(lgbm, train_X, train_y, folds, ord_enc, cat_enc)
 
+# 3. Compare performance and select best model
 models = [lr_param_scores, rf_param_scores, xgb_param_scores, lgbm_param_scores, en_param_scores, lasso_param_scores]
+param_scores = pd.DataFrame(models).sort_values('score', ascending=False)
 
-param_scores = pd.DataFrame(models).sort_values('score',ascending=False)
-
-print(param_scores[['model','score','standard_dev']])
+print(param_scores[['model', 'score', 'standard_dev']])
 
 # take only first row in case of ties (I don't care which model if they're tied)
 champ = param_scores[param_scores.score == param_scores.score.max()].reset_index()
@@ -146,6 +93,7 @@ champ_model = champ.model[0]
 champ_ord_enc = champ.ordinal_encoder[0]
 champ_cat_enc = champ.categorical_encoder[0]
 
+# 4. Read in test data and apply data prep from step 1
 test_df = data_prep.load_data(test_fp)
 test_df = data_prep.eda_clean(test_df)
 test_df, _ = data_prep.clean_after_eda(test_df)
@@ -153,6 +101,7 @@ test_df = data_prep.ordinal_transform(test_df, champ_ord_enc)
 test_df = data_prep.categorical_transform(test_df, champ_cat_enc)
 test_X = test_df if include_categoricals else data_prep.drop_categoricals(test_df)
 
+# 5. predict test data and output to csv
 # the model was trained against log transformed target. Invert log for predictions
 test_y = np.exp(champ_model.predict(test_X))
 print(test_X.head())
@@ -160,5 +109,3 @@ print(test_X.head())
 test_y = pd.DataFrame(test_y, columns=['SalePrice'], index=test_X.index)
 
 test_y.to_csv('prediction.csv')
-
-#print(test_y.head())
