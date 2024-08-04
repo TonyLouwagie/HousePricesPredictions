e072fd8a2961aa09699bedbcea1542064312c58f
Tony Louwagie <tony.louwagie@gmail.com>
2024-08-04 10:10:15 -0500

    bayesian cross validation with random forest (#47)

--- predict_prices/cross_validation.py
+++ predict_prices/cross_validation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_score
+from skopt import BayesSearchCV
 
 
 def cross_val_aggregate(model, X: pd.DataFrame, y: pd.Series, folds: int) -> {str, np.float64}:
@@ -24,3 +25,24 @@ def cross_val_aggregate(model, X: pd.DataFrame, y: pd.Series, folds: int) -> {st
     }
 
     return aggregate_scores
+
+
+def bayes_cross_validation(model, train_X: pd.DataFrame, train_y: pd.Series, param_grid: dict, n_iter: int):
+    """
+    Cross validation with a hyperparameter grid so we can tune to the best hyperparameters for each model
+    :param model:
+    :param train_X:
+    :param train_y:
+    :param param_grid:
+    :param n_iter:
+    :return: best
+    """
+    best = BayesSearchCV(estimator=model,
+                         search_spaces=param_grid,
+                         cv=4,
+                         n_jobs=4,
+                         n_iter=n_iter,
+                         random_state=42)
+    best.fit(train_X, train_y)
+
+    return best
