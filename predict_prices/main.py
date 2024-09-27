d2bc0077a5b82b3284a8e1a6233a55e360805aa7
Tony Louwagie <tony.louwagie@gmail.com>
2024-09-26 19:28:41 -0500

    One function for model scoring (#61)

--- predict_prices/main.py
+++ predict_prices/main.py
@@ -1,11 +1,64 @@
+from dataclasses import dataclass
+from typing import Iterable
+
 import lightgbm
 import numpy as np
 import pandas as pd
-from sklearn import ensemble, linear_model # type: ignore
+from sklearn import ensemble, linear_model  # type: ignore
 import xgboost
 
 import cross_validation
 import data_prep
+
+
+@dataclass
+class ModelParameters:
+    model: any
+    categorical_encoders: data_prep.CategoricalEncoders
+
+@dataclass
+class ModelParameterMap:
+    model_params_map: dict[any, dict[str, Iterable]]
+
+    def measure_model_performance(
+            self,
+            train_data_prep_outputs: data_prep.TrainDataPrepOutputs,
+            n_iter: int,
+            folds: int
+    ) -> ModelParameters:
+
+        models = []
+
+        for i in self.model_params_map:
+            param_grid = self.model_params_map[i]
+
+            if len(param_grid) > 0:
+                best_model = cross_validation.bayes_cross_validation(i, train_data_prep_outputs.train_data, param_grid,
+                                                                     n_iter)
+                best_model_param_scores = cross_validation.save_model_performance_parameters(best_model, folds,
+                                                                                             train_data_prep_outputs)
+            else:
+                i.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
+                best_model_param_scores = cross_validation.save_model_performance_parameters(i, folds,
+                                                                                             train_data_prep_outputs)
+
+            models.append(best_model_param_scores)
+
+            param_scores = pd.DataFrame(models).sort_values('score', ascending=False)
+
+            print(param_scores[['model', 'score', 'standard_dev']])
+
+            # take only first row in case of ties (I don't care which model if they're tied)
+            champ = param_scores[param_scores.score == param_scores.score.max()].reset_index()
+
+            champ_model = champ.model[0]
+            champ_ord_enc = champ.ordinal_encoder[0]
+            champ_cat_enc = champ.categorical_encoder[0]
+
+            champ_parameters = ModelParameters(champ_model, data_prep.CategoricalEncoders(champ_ord_enc, champ_cat_enc))
+
+        return champ_parameters
+
 
 def main():
     train_filepath = "predict_prices/data/train.csv"
@@ -45,62 +98,35 @@ def main():
     ridge = linear_model.Ridge()
     br = linear_model.BayesianRidge()
 
+    model_dict = {
+        lr: [],
+        en: [],
+        lasso: [],
+        ridge: [],
+        br: [],
+        rf: rf_param_grid,
+        xgb: xgb_param_grid,
+        lgbm: lgbm_param_grid
+    }
+    model_param_map = ModelParameterMap(model_dict)
+
     # 1. read in training data, perform data prep
     train_df = data_prep.load_data(train_filepath)
     train_data_prep_inputs = data_prep.TrainDataPrepInputs(train_df, ohe_bool, target_variable, include_categoricals)
     train_data_prep_outputs = train_data_prep_inputs.train_data_prep()
 
     # 2. Measure performance of all of our different models
-    lr.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
-    lr_param_scores = cross_validation.save_model_performance_parameters(lr, folds, train_data_prep_outputs)
+    champ_parameters = model_param_map.measure_model_performance(train_data_prep_outputs, n_iter, folds)
 
-    en.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
-    en_param_scores = cross_validation.save_model_performance_parameters(en, folds, train_data_prep_outputs)
-
-    lasso.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
-    lasso_param_scores = cross_validation.save_model_performance_parameters(lasso, folds, train_data_prep_outputs)
-
-    ridge.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
-    ridge_param_scores = cross_validation.save_model_performance_parameters(ridge, folds, train_data_prep_outputs)
-
-    br.fit(train_data_prep_outputs.train_data.X, train_data_prep_outputs.train_data.y)
-    br_param_scores = cross_validation.save_model_performance_parameters(br, folds, train_data_prep_outputs)
-
-    rf = cross_validation.bayes_cross_validation(rf, train_data_prep_outputs.train_data, rf_param_grid, n_iter)
-    rf_param_scores = cross_validation.save_model_performance_parameters(rf, folds, train_data_prep_outputs)
-
-    xgb = cross_validation.bayes_cross_validation(xgb, train_data_prep_outputs.train_data, xgb_param_grid, n_iter)
-    xgb_param_scores = cross_validation.save_model_performance_parameters(xgb, folds, train_data_prep_outputs)
-
-    lgbm = cross_validation.bayes_cross_validation(lgbm, train_data_prep_outputs.train_data, lgbm_param_grid, n_iter)
-    lgbm_param_scores = cross_validation.save_model_performance_parameters(lgbm, folds, train_data_prep_outputs)
-
-    # 3. Compare performance and select best model
-    models = [lr_param_scores, rf_param_scores, xgb_param_scores, lgbm_param_scores, en_param_scores, lasso_param_scores,
-              ridge_param_scores, br_param_scores]
-    param_scores = pd.DataFrame(models).sort_values('score', ascending=False)
-
-    print(param_scores[['model', 'score', 'standard_dev']])
-
-    # take only first row in case of ties (I don't care which model if they're tied)
-    champ = param_scores[param_scores.score == param_scores.score.max()].reset_index()
-    champ_model = champ.model[0]
-    champ_ord_enc = champ.ordinal_encoder[0]
-    champ_cat_enc = champ.categorical_encoder[0]
-
-    # 4. Read in test data and apply data prep from step 1
+    # 3. Read in test data and apply data prep from step 1
     test_df = data_prep.load_data(test_filepath)
-    champ_categorical_encoders = data_prep.CategoricalEncoders(champ_ord_enc, champ_cat_enc)
-    test_data_prep_inputs = data_prep.TestDataPrepInputs(test_df, champ_categorical_encoders, include_categoricals)
+    test_data_prep_inputs = data_prep.TestDataPrepInputs(test_df, champ_parameters.categorical_encoders, include_categoricals)
     test_X = test_data_prep_inputs.test_data_prep()
 
-    # 5. predict test data and output to csv
+    # 4. predict test data and output to csv
     # the model was trained against log transformed target. Invert log for predictions
-    test_y = np.exp(champ_model.predict(test_X))
-    print(test_X.head())
-
+    test_y = np.exp(champ_parameters.model.predict(test_X))
     test_y = pd.DataFrame(test_y, columns=[target_variable], index=test_X.index)
-
     test_y.to_csv('prediction.csv')
 
 
